c80d16d75e1ca4d6e88d9ac7d1e2e4cb3add1716
GizmoGear22 <mpsimmer@gmail.com>
2024-07-17 22:29:28 -0400

    Added ValidationDelegate

---
+++ DelegateUtilities/ValidationDelegate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateUtilities
+{
+	public class ValidationDelegate
+	{
+		public delegate bool DelegateForValidation(CardModel model);
+		public delegate Task ValidationMessageDelegate(string message);
+	}
+}
