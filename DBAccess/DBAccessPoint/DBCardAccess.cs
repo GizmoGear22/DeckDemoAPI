4bff4907bd16b821af43b273f830f00910875a26
GizmoGear22 <mpsimmer@gmail.com>
2024-07-17 22:36:05 -0400

    Added DBCardAccess

---
+++ DBAccess/DBAccessPoint/DBCardAccess.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+
+namespace DBAccess.DBAccessPoint
+{
+	public class DBCardAccess : IDBCardAccess
+	{
+		private readonly IConfiguration _configuration;
+		public DBCardAccess(IConfiguration configuration)
+		{
+			_configuration = configuration;
+		}
+		public string CnnVal()
+		{
+			return _configuration.GetConnectionString("DBAccessHandler");
+		}
+
+		public async Task<List<T>> DBGetConnectionHandler<T>(string sqlString)
+		{
+			using (var connection = new SqlConnection(CnnVal()))
+				try
+				{
+					var data = await connection.QueryAsync<T>(sqlString);
+					return data.ToList();
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine(ex.Message);
+					throw ex;
+				}
+
+		}
+		public async Task<List<T>> DBGetConnectionHandlerByType<T>(string sqlString, CardType param)
+		{
+			using (var connection = new SqlConnection(CnnVal()))
+			{
+				var parameter = new { type = param };
+				var data = await connection.QueryAsync<T>(sqlString, parameter);
+				return data.ToList();
+			}
+		}
+
+		public async Task<T> GetConnectionHandlerById<T>(string sqlString, int Id)
+		{
+			using (var connection = new SqlConnection(CnnVal()))
+			{
+				try
+				{
+					var parameter = new { id = Id };
+					var data = await connection.QueryFirstOrDefaultAsync<T>(sqlString, parameter);
+					return data;
+				}
+				catch (Exception ex)
+				{
+					throw new Exception(ex.Message);
+				}
+			}
+		}
+
+		public async Task<int> DBPostConnectionHandler(string sqlString, object param)
+		{
+			using (var connection = new SqlConnection(CnnVal()))
+			{
+				try
+				{
+					var data = await connection.ExecuteAsync(sqlString, param);
+					return data;
+				}
+				catch (Exception ex)
+				{
+					throw new Exception(ex.Message);
+				}
+
+
+			}
+		}
+	}
+}
---
+++ DBAccess/DBAccessPoint/IDBCardAccess.cs
@@ -0,0 +1,12 @@
+﻿
+namespace DBAccess.DBAccessPoint
+{
+	public interface IDBCardAccess
+	{
+		string CnnVal();
+		Task<List<T>> DBGetConnectionHandler<T>(string sqlString);
+		Task<List<T>> DBGetConnectionHandlerByType<T>(string sqlString, CardType param);
+		Task<int> DBPostConnectionHandler(string sqlString, object param);
+		Task<T> GetConnectionHandlerById<T>(string sqlString, int Id);
+	}
+}
