3a0f278f9eeff531d49bb664672cf1415fcb3e0b
GizmoGear22 <mpsimmer@gmail.com>
2024-07-17 22:15:53 -0400

    Added DBGetLogic

---
+++ LogicLayer/DBGetLogic/DBGetHandlers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+
+namespace LogicLayer.DBGetLogic
+{
+	internal class DBGetHandlers : IDBGetHandlers
+	{
+		private readonly IAvailableCardsController _availableCardsController;
+		public DBGetHandlers(IAvailableCardsController availableCardsController)
+		{
+			_availableCardsController = availableCardsController;
+		}
+
+		public async Task<IEnumerable<CardModel>> GetAllCardsFromRepository()
+		{
+			var allCardData = await _availableCardsController.SeeAllCardOptions();
+			return allCardData;
+		}
+
+
+		public async Task<IEnumerable<CardModel>> GetAllCardsByTypeRepository(CardType type)
+		{
+			var data = await _availableCardsController.SeeCardOptionsByType(type);
+			return data.ToList();
+		}
+
+		public async Task<CardModel> GetCardByIdFromRepository(int id)
+		{
+			var data = await _availableCardsController.SeeCardById(id);
+			return data;
+		}
+	}
+}
---
+++ LogicLayer/DBGetLogic/IDBGetHandlers.cs
@@ -0,0 +1,9 @@
+﻿namespace LogicLayer.DBGetLogic
+{
+	internal interface IDBGetHandlers
+	{
+		Task<IEnumerable<CardModel>> GetAllCardsByTypeRepository(CardType type);
+		Task<IEnumerable<CardModel>> GetAllCardsFromRepository();
+		Task<CardModel> GetCardByIdFromRepository(int id);
+	}
+}
