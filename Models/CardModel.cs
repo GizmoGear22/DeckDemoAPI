92f9b30ffa9bd5942fd9c5415f2e6c54fa9eee6a
GizmoGear22 <mpsimmer@gmail.com>
2024-07-17 22:32:18 -0400

    Added CardModel

---
+++ Models/CardModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+
+namespace Models
+{
+	internal class CardModel
+	{
+		public int id { get; set; }
+		public string name { get; set; }
+		public CardType type { get; set; }
+		public int cost { get; set; }
+		public int attack { get; set; }
+		public int defense { get; set; }
+		public string typeString => type.ToString();
+		public string inputType { get; set; }
+	}
+
+	public enum CardType
+	{
+		Machine,
+		Pyro,
+		Alchemy,
+		Tesla,
+		Bio,
+		Blank
+	}
+}
