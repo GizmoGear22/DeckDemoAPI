4b79a228aa14a3291219dcb7ee1804130b37aee1
GizmoGear22 <mpsimmer@gmail.com>
2024-07-25 15:30:48 -0400

    Created a validation for name

---
+++ LogicLayer/Validation/CheckName/AdjustName.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Models;
+
+namespace LogicLayer.Validation.CheckName
+{
+	public class AdjustName
+	{
+		public CardModel CapitalizeFirstCharacter(CardModel card)
+		{
+			if (char.IsLower(card.name[0]))
+			{
+				card.name =  card.name.ToUpper();
+				return card;
+			}
+			else { return card; }
+		}
+	}
+}
